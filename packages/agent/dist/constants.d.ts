ff427674d8a874538c09d73536aa4443982b56b3
Viktor Zhakhalov <zhakhalov@gmail.com>
2023-12-13 17:25:18 +0200

    update action prompt

--- packages/agent/dist/constants.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const ACTION_SEP = "\n\n";
-export declare const ACTIVITY_SEP = "\n";
-export declare const TEMPLATE: string;
--- packages/agent/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './action';
-export * from './agent';
-export * from './constants';
-export * from './activity';
-export * from './types';
